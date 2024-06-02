1ee4429f3fb7fd8d31c412140663b2365cc1fb0c
lpc421 <166678996+lpc421@users.noreply.github.com>
2024-06-02 16:51:40 +0800

    Add files via upload

---
+++ 4.11.1,4.11.2.py
@@ -0,0 +1,14 @@
+def f(x,n):
+    return x ** n
+
+print(f(2,5))
+
+
+def is_odd(x):
+    if x % 2 == 1:
+        return True
+    else:
+        return False
+    
+print(is_odd(35))
+print(is_odd(546))
